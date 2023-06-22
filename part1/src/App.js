c8a7ac47e5d38e4e4907d21b4ffd42f719faa602
sarah-die <saridi@alice.de>
2023-06-22 11:46:41 +0200

    1.1: initial commit

--- part1/src/App.js
+++
@@ -1,8 +0,0 @@
-// an arrow function is assigned to App. Note: here in shortform because the function only consists of a single expression
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-);
-
-export default App;
