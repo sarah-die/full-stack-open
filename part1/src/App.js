ae8873b084248ca8802d3cfbbdac0feb273ce851
sarah-die <saridi@alice.de>
2023-06-22 10:56:12 +0200

    installed prettier

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,8 @@
+// an arrow function is assigned to App. Note: here in shortform because the function only consists of a single expression
 const App = () => (
-    <div>
-      <p>Hello world</p>
-    </div>
-)
+  <div>
+    <p>Hello world</p>
+  </div>
+);
 
-export default App
+export default App;
