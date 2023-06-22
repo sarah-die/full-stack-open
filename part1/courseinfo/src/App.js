ce28ffe3de7753e24294cb70f59fdc79bf194fb0
sarah-die <saridi@alice.de>
2023-06-22 12:42:46 +0200

    1.3: course information step 3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -14,7 +14,7 @@ const Content = (props) => {
   return (
     <div>
       {props.content.map((el, index) => {
-        return <Part part={el.part} exercise={el.exercise} key={index} />;
+        return <Part part={el.name} exercise={el.exercises} key={index} />;
       })}
     </div>
   );
@@ -26,23 +26,25 @@ const Total = (props) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
-  const content = [
-    { part: part1, exercise: exercises1 },
-    { part: part2, exercise: exercises2 },
-    { part: part3, exercise: exercises3 },
-  ];
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
+  const content = [part1, part2, part3];
 
   return (
     <div>
       <Header course={course} />
       <Content content={content} />
-      <Total sum={exercises1 + exercises2 + exercises3} />
+      <Total sum={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   );
 };
