933812228d6fbcc019ca8c8db6ba228a28d9eba3
sarah-die <saridi@alice.de>
2023-06-23 11:53:00 +0200

    (2.3*): already in the recommended way

--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -1,4 +1,4 @@
 export const Total = ({ parts }) => {
   const sum = parts.reduce((acc, obj) => acc + obj.exercises, 0);
-  return <h3>Total of exercises: {sum}</h3>;
+  return <h3>Total of exercises: {sum} </h3>;
 };
