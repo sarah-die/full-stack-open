5ffa077d70f8b2b8a79aacafdbab37d07dd39313
sarah-die <saridi@alice.de>
2023-06-23 12:45:40 +0200

    2.6: Phonebook Step 1

---
+++ part2/phonebook/src/App.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+type Contact = { name: string; id: number };
+
+const App = () => {
+  const [persons, setPersons] = useState<Contact[]>([
+    { name: "Arto Hellas", id: 1 },
+  ]);
+  // to control the form input element
+  const [newName, setNewName] = useState<string>("");
+
+  const addPerson = (event: any) => {
+    event.preventDefault();
+    const personObject: Contact = {
+      name: newName,
+      id: persons.length + 1,
+    };
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
+
+  const handleNameChange = (event: any) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson} name={"Contact"}>
+        <div>
+          name:{" "}
+          <input
+            value={newName}
+            onChange={handleNameChange}
+            name={"Contact-Input"}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => {
+        return <div key={person.name}>{person.name}</div>;
+      })}
+    </div>
+  );
+};
+
+export default App;
