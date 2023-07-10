0acd66cf908d5a2b9149da0c7984e6d5df473295
sarah-die <saridi@alice.de>
2023-07-10 11:34:45 +0200

    4.10: Blog list tests, step 3

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -32,6 +32,31 @@ test('unique identifier is named id and exists', async () => {
   expect(response.body[0].id).toBeDefined();
 });
 
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+  };
+
+  // verify that content is saved correctly to db
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  // total number is increased by one
+  const blogsAtEnd = await helper.blogsInDb();
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+  // verify that content is saved correctly to db
+  const contents = blogsAtEnd.map((b) => b);
+  console.log('contents', contents);
+  expect(contents[contents.length - 1]).toMatchObject(newBlog);
+});
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
