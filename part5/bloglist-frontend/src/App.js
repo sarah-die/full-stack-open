c12f9f728a49f978101ad7dbdec4f2d160cf212b
sarah-die <saridi@alice.de>
2023-07-14 09:52:40 +0200

    5.4: Bloglist Frontend, step 4 (display notifications)

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -7,11 +7,16 @@ import NewBlogForm from "./components/NewBlogForm";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
+  const [newBlog, setNewBlog] = useState({ title: "", author: "", url: "" });
+
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
 
-  const [newBlog, setNewBlog] = useState({ title: "", author: "", url: "" });
+  const [notification, setNotification] = useState(
+    "Notifications appear here."
+  );
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
@@ -26,34 +31,65 @@ const App = () => {
     }
   }, []);
 
+  useEffect(() => {
+    if (notification === "") {
+      setIsVisible(false);
+      return;
+    }
+    setIsVisible(true);
+    const timer = setTimeout(() => {
+      setIsVisible(false);
+      setNotification("");
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, [notification]);
+
   const handleLogin = async (event) => {
     event.preventDefault();
-    const user = await loginService.login({ username, password });
-    blogService.setToken(user.token);
-    window.localStorage.setItem("loggedBlogAppUser", JSON.stringify(user));
-    setUser(user);
-    setUsername("");
-    setPassword("");
+    try {
+      const user = await loginService.login({ username, password });
+      blogService.setToken(user.token);
+      window.localStorage.setItem("loggedBlogAppUser", JSON.stringify(user));
+      setUser(user);
+      setNotification(`Login successful for user "${username}"`);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      setNotification(error.response.data.error);
+    }
   };
 
   const handleLogout = async (event) => {
     event.preventDefault();
-    window.localStorage.removeItem("loggedBlogAppUser");
-    setUser(null);
+    try {
+      window.localStorage.removeItem("loggedBlogAppUser");
+      setNotification(`Logout successful`);
+      setUser(null);
+    } catch (error) {
+      setNotification(error.response.data.error);
+    }
   };
 
   const addBlog = async (event) => {
     event.preventDefault();
 
-    const returnedBlog = await blogService.create(newBlog);
-    setBlogs(blogs.concat(returnedBlog));
-    setNewBlog({ title: "", author: "", url: "" });
+    try {
+      const returnedBlog = await blogService.create(newBlog);
+      setBlogs(blogs.concat(returnedBlog));
+      setNotification(
+        `A new blog ${returnedBlog.title} by ${returnedBlog.author} added`
+      );
+      setNewBlog({ title: "", author: "", url: "" });
+    } catch (error) {
+      setNotification(error.response.data.error);
+    }
   };
 
   if (user === null) {
     return (
       <div>
         <h2>Log in to application</h2>
+        {isVisible && <div className="error">{notification}</div>}
         <LoginForm
           handleLogin={handleLogin}
           setPassword={setPassword}
@@ -68,6 +104,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
+      {isVisible && <div className="error">{notification}</div>}
       <div>{user.username} logged in</div>
       <button onClick={handleLogout}>logout</button>
       <h2>Create new Blog</h2>
