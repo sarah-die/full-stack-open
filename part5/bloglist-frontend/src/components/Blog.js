12921b3584f8a488940991c6f6f39eaed0c802cd
sarah-die <saridi@alice.de>
2023-07-14 14:54:42 +0200

    5.7: Blog list frontend, step 7 (hide and view blog details)

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,37 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+      </div>
+      {isVisible ? (
+        <button onClick={() => setIsVisible(!isVisible)}>hide</button>
+      ) : (
+        <button onClick={() => setIsVisible(!isVisible)}>view</button>
+      )}
+      {isVisible && (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            <div>likes {blog.likes}</div>
+            <button>like</button>
+          </div>
+          <div>{blog.username}</div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
