12bd57cdc989c051091a56ba04eac3b2ab7b2e50
sarah-die <saridi@alice.de>
2023-07-17 17:23:08 +0200

    5.14: Blog list tests, step 2 (user events)

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import Blog from './Blog';
+import userEvent from '@testing-library/user-event/';
 
 test('<Blog/> renders title and author but does not render URL or number of likes by default', () => {
   const blog = {
@@ -19,4 +20,30 @@ test('<Blog/> renders title and author but does not render URL or number of like
   expect(div).toHaveTextContent('test author');
   expect(div).not.toHaveTextContent('test url');
   expect(div).not.toHaveTextContent('25');
+});
+
+test('<Blog/> URL and likes are shown when button is clicked', async () => {
+  const blog = {
+    title: 'test title',
+    author: 'test author',
+    url: 'test url',
+    likes: 25,
+    user: { username: 'test user' },
+  };
+
+  const testUser = {
+    username: 'test user',
+    user: 'test user',
+  };
+
+  const { container } = render(<Blog blog={blog} user={testUser} />);
+
+  const user = userEvent.setup();
+  const button = screen.getByText('view');
+  await user.click(button);
+
+  const div = container.querySelector('.blogElement');
+
+  expect(div).toHaveTextContent('test url');
+  expect(div).toHaveTextContent('25');
 });
