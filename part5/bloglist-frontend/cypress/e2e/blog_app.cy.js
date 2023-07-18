cdf9b9719fb8755a24ecff26968541372f679604
sarah-die <saridi@alice.de>
2023-07-18 17:01:25 +0200

    5.18: Bloglist end to end testing, step 2 (test login)

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,19 +1,38 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset');
-    cy.visit('http://localhost:3000');
 
-    // const user = {
-    //   name: 'testuser',
-    //   username: 'testuser',
-    //   password: 'password',
-    // };
-    // cy.request('POST', '', user);
+    const user = {
+      name: 'testuser',
+      username: 'testuser',
+      password: 'password',
+    };
+    cy.request('POST', 'http://localhost:3003/api/users', user);
+
+    cy.visit('http://localhost:3000');
   });
 
   it('Login form is shown', function () {
     cy.contains('Log in to application');
     cy.contains('username');
     cy.contains('password');
+  });
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('testuser');
+      cy.get('#password').type('password');
+      cy.get('#login-button').click();
+
+      cy.contains('Login successful for user "testuser"');
+    });
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('testuser');
+      cy.get('#password').type('wrong');
+      cy.get('#login-button').click();
+
+      cy.contains('invalid username or password');
+    });
   });
 });
