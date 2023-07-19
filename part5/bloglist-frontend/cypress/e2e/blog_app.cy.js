2bad38f67ab4ac55a94b953cc0e0e1730b674ccf
sarah-die <saridi@alice.de>
2023-07-19 09:08:38 +0200

    5.23: Bloglist end to end testing, step 7 (check order from array elements)

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -105,5 +105,37 @@ describe('Blog app', function () {
         cy.get('#delete-button').should('not.exist');
       });
     });
+
+    describe('and several blogs exits with a different number of likes', function () {
+      beforeEach(function () {
+        cy.contains('create Blog').click();
+        cy.get('#title').type('Title with the second most likes');
+        cy.get('#author').type('Ursula Poznanski');
+        cy.get('#url').type('www.ere.bos');
+        cy.get('#create-blog-button').click();
+
+        cy.contains('create Blog').click();
+        cy.get('#title').type('Title with the most likes');
+        cy.get('#author').type('Ursula Poznanski');
+        cy.get('#url').type('www.ere.bos');
+        cy.get('#create-blog-button').click();
+
+        cy.get('.blogElement').eq(1).as('mostLikes');
+        cy.get('@mostLikes').within(() => {
+          cy.get('#view-button').click();
+          cy.get('#like-button').click();
+        });
+      });
+
+      it('and they are ordered according to likes with the most liked blog first', function () {
+        cy.get('.blogElement')
+          .eq(0)
+          .should('contain', 'Title with the most likes');
+
+        cy.get('.blogElement')
+          .eq(1)
+          .should('contain', 'Title with the second most likes');
+      });
+    });
   });
 });
