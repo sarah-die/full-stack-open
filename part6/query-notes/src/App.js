91ca9f163363a7ab56c758f21bca1b4417b7f40f
sarah-die <saridi@alice.de>
2023-07-25 15:21:35 +0200

    Part 6 d): Until 6.20

--- part6/query-notes/src/App.js
+++ part6/query-notes/src/App.js
@@ -1,32 +1,62 @@
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import { getNotes, createNote, updateNote } from "./requests";
+
 const App = () => {
+  const queryClient = useQueryClient();
+
+  // create new note
+  // parameter is function that uses Axios to send a new note to the server
+  // successfull mutation: invalidateQueries is called -> automatically update a query with the key "notes" (see below)
+  // i.e. fetch notes -> as a result app renders the up-to-date state on the server & added note is also rendered
+  const newNoteMutation = useMutation(createNote, {
+    onSuccess: (newNote) => {
+      // queryClient.invalidateQueries("notes");
+      // manually updating the query state
+      const notes = queryClient.getQueryData("notes");
+      queryClient.setQueryData("notes", notes.concat(newNote));
+    },
+  });
+
   const addNote = async (event) => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-    console.log(content)
-  }
+    event.preventDefault();
+    const content = event.target.note.value;
+    event.target.note.value = "";
+    // call the mutation objects function "mutate" and pass the new note as parameter
+    newNoteMutation.mutate({ content, important: true });
+  };
+
+  const updateNoteMutation = useMutation(updateNote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("notes");
+    },
+  });
 
   const toggleImportance = (note) => {
-    console.log('toggle importance of', note.id)
-  }
+    updateNoteMutation.mutate({ ...note, important: !note.important });
+  };
 
-  const notes = []
+  // axios call is wrapped in a query formed with useQuery function
+  // no use of useEffect / useState
+  const result = useQuery("notes", getNotes);
+  console.log(result);
 
-  return(
+  const notes = result.data || [];
+
+  return (
     <div>
       <h2>Notes app</h2>
       <form onSubmit={addNote}>
         <input name="note" />
         <button type="submit">add</button>
       </form>
-      {notes.map(note =>
+      {notes.map((note) => (
         <li key={note.id} onClick={() => toggleImportance(note)}>
-          {note.content} 
-          <strong> {note.important ? 'important' : ''}</strong>
+          {note.content}
+          <strong> {note.important ? "important" : ""}</strong>
         </li>
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- part6/query-notes/src/index.js
+++ part6/query-notes/src/index.js
@@ -1,8 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-import App from './App'
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+// pass react query library functions to the entire application
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
