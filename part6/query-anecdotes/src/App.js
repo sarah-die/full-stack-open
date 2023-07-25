d6880236fecee3473f1c6d16a438ff462d4b6b59
sarah-die <saridi@alice.de>
2023-07-25 16:26:36 +0200

    6.20: Retrieving anecdotes from server using React Query

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -1,40 +1,42 @@
-import AnecdoteForm from './components/AnecdoteForm'
-import Notification from './components/Notification'
+import AnecdoteForm from "./components/AnecdoteForm";
+import Notification from "./components/Notification";
+import { useQuery, useQueryClient } from "react-query";
+import { getAnecdotes } from "./requests";
 
 const App = () => {
+  const queryClient = useQueryClient();
 
   const handleVote = (anecdote) => {
-    console.log('vote')
+    console.log("vote");
+  };
+
+  const result = useQuery("anecdotes", getAnecdotes, { retry: false });
+  console.log(result);
+
+  if (result.status === "error") {
+    return <div>Anecdote service not available due to problems in server</div>;
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const anecdotes = result.data || [];
 
   return (
     <div>
       <h3>Anecdote app</h3>
-    
+
       <Notification />
       <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
+
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => handleVote(anecdote)}>vote</button>
           </div>
         </div>
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
